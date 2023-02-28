9772609e60903c5f48c8b60acff3d897188de79a
zoulikha <zaz13@me.com>
2023-02-28 15:11:35 +0100

    creation bouton

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -25,11 +25,22 @@ async function works() {
 }
 works();
 
+const divMenu = document.querySelector("#menu-bouton");
+
 //Appel de l'API catégories
 async function categories() {
   const response = await fetch("http://localhost:5678/api/categories");
   const listeCategories = await response.json();
   console.log(listeCategories);
+
+  for (let index = 0; index < listeCategories.length; index++) {
+    const category = listeCategories[index];
+
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = category.name;
+    buttonElement.className = "button-category";
+    divMenu.appendChild(buttonElement);
+  }
 }
 
 categories();
