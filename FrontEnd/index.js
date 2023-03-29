e6f35ac568a715aa6e7e02627193d1f368f080d7
zoulikha <zaz13@me.com>
2023-03-29 19:54:33 +0200

    integration modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,16 +6,16 @@ async function works() {
   const response = await fetch("http://localhost:5678/api/works");
   listeTravaux = await response.json();
   console.log(listeTravaux);
-  afficheWorks(listeTravaux);
+  afficheWorks();
 }
 
-function afficheWorks(travauxAAfficher) {
+function afficheWorks() {
   divGallery.innerHTML = "";
   // Entourer notre code avec une boucle for.
   // Cela permet de créer toutes les fiches produits de notre site.
-  for (let i = 0; i < travauxAAfficher.length; i++) {
+  for (let i = 0; i < listeTravaux.length; i++) {
     //Création des balises
-    const projet = travauxAAfficher[i];
+    const projet = listeTravaux[i];
 
     const figureElement = document.createElement("figure");
     divGallery.appendChild(figureElement);
@@ -87,4 +87,93 @@ if (localStorage.getItem("token")) {
   document.querySelector(".btn-modifier1").style.display = "none";
   document.querySelector(".btn-modifier2").style.display = "none";
   document.querySelector(".lien-logout").style.display = "none";
+}
+
+// -------------------------------------- fenêtre modale ----------------------------------------
+//Ouverture de la modale
+const openModal = function (event) {
+  event.preventDefault();
+  document.querySelector(".modale").style.display = null; // Permet de retirer le display none que j'ai mis
+  afficheWorksMini();
+};
+
+document.querySelector(".btn-modifier2").addEventListener("click", openModal);
+
+//Fermeture de la modale
+//Permet la propagation de l'evenement vers les parents
+const stopPropagation = function (event) {
+  event.stopPropagation();
+};
+
+const closeModal = function (event) {
+  event.preventDefault();
+  document.querySelector(".modale").style.display = "none";
+};
+
+document.querySelector(".modale").addEventListener("click", closeModal);
+document.querySelector(".btn-close").addEventListener("click", closeModal);
+document
+  .querySelector(".js-modal-stop")
+  .addEventListener("click", stopPropagation);
+
+// ------------------------------------------ Faire apparaître les projets ds Modale --------------------------------
+function afficheWorksMini() {
+  const editGallery = document.querySelector("#edit-gallery");
+  editGallery.innerHTML = "";
+  // Entourer notre code avec une boucle for.
+  // Cela permet de créer toutes les fiches produits de notre site.
+  for (let i = 0; i < listeTravaux.length; i++) {
+    //Création des balises
+    const projet = listeTravaux[i];
+
+    const divElement = document.createElement("div");
+    editGallery.appendChild(divElement);
+    divElement.classList.add("premier-work");
+    divElement.style.backgroundImage = `url(${projet.imageUrl})`;
+
+    const divButtonElement = document.createElement("div");
+    divElement.appendChild(divButtonElement);
+    divButtonElement.classList.add("btn-work");
+
+    const projectButtonElement = document.createElement("button");
+    divButtonElement.appendChild(projectButtonElement);
+    projectButtonElement.classList.add("btn-projet");
+
+    const iconeEditer = document.createElement("i");
+    projectButtonElement.appendChild(iconeEditer);
+    iconeEditer.classList.add("fa-light", "fa-arrows-up-down-left-right");
+    iconeEditer.classList.add("icone-editer");
+
+    const deleteButtonElement = document.createElement("button");
+    divButtonElement.appendChild(deleteButtonElement);
+    deleteButtonElement.classList.add("btn-delete");
+    deleteButtonElement.addEventListener("click", () => deletework(projet.id));
+
+    const iconeDelete = document.createElement("i");
+    deleteButtonElement.appendChild(iconeDelete);
+    iconeDelete.classList.add("fa-solid", "fa-trash-can", "fa-xs");
+    iconeDelete.classList.add("icone-delete");
+
+    const editButtonElement = document.createElement("button");
+    divElement.appendChild(editButtonElement);
+    editButtonElement.classList.add("btn-editer");
+    editButtonElement.innerText = "éditer";
+  }
+}
+
+async function deletework(id) {
+  console.log(id);
+  const confirmDelete = confirm(
+    "Êtes-vous sûr de bien vouloir supprimer ce projet ?"
+  );
+  if (confirmDelete) {
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+    });
+    const deleteResponse = await response.json();
+    console.log(deleteResponse);
+  }
 }
